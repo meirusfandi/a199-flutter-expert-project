3aa84d1f90f38d5fa73acf41d94c12604d646f59
Mei Rusfandi <mei.rusfandi@dto.kemkes.go.id>
2025-04-08 20:20:49 +0700

    fixing filter posterPath == null on tv response

--- lib/data/models/tv_response.dart
+++ lib/data/models/tv_response.dart
@@ -7,8 +7,7 @@ class TvResponse extends Equatable {
   TvResponse({required this.tvList});
 
   factory TvResponse.fromJson(Map<String, dynamic> json) => TvResponse(tvList: List<TvModel>.from((json["results"] as List)
-    .map((x) => TvModel.fromJson(x))
-    .where((element) => element.posterPath != null))
+    .map((x) => TvModel.fromJson(x)))
   );
 
   Map<String, dynamic> toJson() => {
